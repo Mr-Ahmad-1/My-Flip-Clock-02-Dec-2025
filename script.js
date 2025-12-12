d6e861f13f513a35b981b5281897a8c1aa37b02c
Ahmad Raza <mrazeem.oo009@gmail.com>
2025-12-12 19:29:40 +0500

    random_background

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ flipClock({
     })
   })
 });
+
+const images = [
+    "./bmw_m3_bmw_car.jpg",
+    "./_dark.jpg",
+    "./bmw_logo_drops.jpg"
+  ];
+
+  // Pick a random one
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
+  // Set it as page background
+  document.body.style.backgroundImage = `url(${randomImage})`;
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
